e83bf1d4c25cd9640a17d9396b249eb8297f8a98
geunheejung <geunhee0212@gmail.com>
2023-09-30 09:10:57 +0900

    axios def

--- src/axiosStudy.ts
+++ src/axiosStudy.ts
@@ -1,7 +1,61 @@
 import axios, { AxiosError } from "axios";
 
-// axios는 get, post의 따라서 다른 파라미터와 리턴값을 가진다.
-// axios
+interface R<T = any> {
+  data: T;
+}
+
+interface E {
+  response?: R;
+  message: string;
+}
+
+interface confi<T = any> {
+  url: string;
+  method: string;
+  headers?: {};
+  body?: T;
+}
+
+class Axi {
+  get() {}
+  post() {}
+  put() {}
+}
+
+interface AxInterface extends Axi {
+  <T = any, J = R>(axiosConfig: confi<T>): Promise<R<J>>;
+}
+
+interface AxStatic extends AxInterface {
+  Axios: typeof Axi;
+}
+
+declare const ax: AxStatic;
+
+// const fn: AxInterface = <T, J>(config: confi): Promise<J> => {
+//   return new Promise((resolve, reject) => {
+//     try {
+//       const res = fetch(config.url, {  method: config.method, body: config.body });
+//       return res;
+//     } catch (error ) {
+//       reject(error);
+//     }
+//   });
+// }
+
+// 1. 함수로서 호출하는 경우
+ax({ url: "", method: "" });
+
+// 2. 메서드로서 호출하는 경우
+ax.get();
+ax.post();
+ax.put();
+
+// 3. 클래스로서 인스턴스화 하는 경우
+const instnace = new axios();
+
+// 4. create 팩토리 메서드를 통해 생성하는 경우
+const factory = axios.create();
 
 interface IResponse<T = {}> {
   data: T;
@@ -28,7 +82,7 @@ axios
   .get("")
   .then()
   .catch((error: unknown) => {
-    if (CustomError.isCustomError<IResponse<IUser>>(error)) {
+    if (axios.isAxiosError<IUser>(error)) {
       // console.error(
       //   (error as  AxiosError<{ message: string }>.response?.data.message)
       // )
